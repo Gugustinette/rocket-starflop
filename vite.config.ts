5b67db0f5a167f409ac2bc165956cc0e2d3aa2c6
Gugustinette <mercier.augustin@outlook.fr>
2024-10-23 10:56:47 +0200

    chore: update project config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     wasm(),
     topLevelAwait(),
   ],
-  base: '/starter-3d',
+  base: '/rocket-starflop',
   build: {
     rollupOptions: {
       // Solution found here: https://github.com/dimforge/rapier.js/issues/278
