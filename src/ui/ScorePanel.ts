996826388e73fc5d17a81f6ae4b05b52aeeab8fc
Gugustinette <mercier.augustin@outlook.fr>
2024-10-27 02:25:56 +0200

    fix: score panel user select

--- src/ui/ScorePanel.ts
+++ src/ui/ScorePanel.ts
@@ -13,6 +13,7 @@ export class ScorePanel {
     this.__DOM_SCORE__.innerText = 'Score: 0';
     this.__DOM__.appendChild(this.__DOM_SCORE__);
     // Style
+    this.__DOM__.style.userSelect = 'none';
     this.__DOM__.style.position = 'absolute';
     this.__DOM__.style.top = '10px';
     this.__DOM__.style.right = '20px';
