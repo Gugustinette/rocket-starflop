d34ffa8de7f09726db3a874830d1451f3e2f1add
Gugustinette <mercier.augustin@outlook.fr>
2024-11-01 00:42:50 +0100

    feat: add secondary UIs + add credits content

--- src/ui/Menu.ts
+++ src/ui/Menu.ts
@@ -9,6 +9,10 @@ export class Menu {
   scoresButton: Button;
   creditsButton: Button;
 
+  __DOM_SCORES__: HTMLElement;
+
+  __DOM_CREDITS__: HTMLElement;
+
   constructor() {
     // Create DOM
     this.__DOM__ = document.createElement('div');
@@ -20,6 +24,7 @@ export class Menu {
     this.__DOM_LOGO__.style.maxWidth = '600px';
     this.__DOM_LOGO__.style.height = 'auto';
     this.__DOM_LOGO__.style.marginBottom = '80px';
+    this.__DOM_LOGO__.style.transition = 'all 0.5s ease';
     this.__DOM__.appendChild(this.__DOM_LOGO__);
     // Create Play button
     this.playButton = new Button('Jouer');
@@ -36,12 +41,6 @@ export class Menu {
       }, 500);
     })
     this.__DOM__.appendChild(this.playButton.__DOM__);
-    // Create Scores button
-    this.scoresButton = new Button('Scores');
-    this.__DOM__.appendChild(this.scoresButton.__DOM__);
-    // Create Credits button
-    this.creditsButton = new Button('Crédits');
-    this.__DOM__.appendChild(this.creditsButton.__DOM__);
     // Style
     this.__DOM__.style.userSelect = 'none';
     this.__DOM__.style.position = 'absolute';
@@ -68,6 +67,94 @@ export class Menu {
         this.__DOM__.style.opacity = '1';
       }
     });
+
+    const loadSecondaryPanel = () => {
+      this.__DOM__.style.justifyContent = 'flex-start';
+      this.__DOM_LOGO__.style.marginBottom = '10px';
+      this.__DOM_LOGO__.style.maxWidth = '300px';
+      this.playButton.__DOM__.style.display = 'none';
+      this.scoresButton.__DOM__.style.display = 'none';
+      this.creditsButton.__DOM__.style.display = 'none';
+    }
+    const hideSecondaryPanel = () => {
+      this.__DOM__.style.justifyContent = 'center';
+      this.__DOM_LOGO__.style.marginBottom = '80px';
+      this.__DOM_LOGO__.style.maxWidth = '600px';
+      this.__DOM_SCORES__.style.display = 'none';
+      this.__DOM_CREDITS__.style.display = 'none';
+      this.playButton.__DOM__.style.display = 'block';
+      this.scoresButton.__DOM__.style.display = 'block';
+      this.creditsButton.__DOM__.style.display = 'block';
+    }
+
+    // Create Scores DOM
+    this.__DOM_SCORES__ = document.createElement('div');
+    this.__DOM_SCORES__.innerHTML = 'Scores';
+    this.__DOM_SCORES__.style.display = 'none';
+    this.__DOM_SCORES__.style.flexDirection = 'column';
+    this.__DOM_SCORES__.style.alignItems = 'center';
+    this.__DOM_SCORES__.style.justifyContent = 'center';
+    this.__DOM_SCORES__.style.gap = '20px';
+    this.__DOM__.appendChild(this.__DOM_SCORES__);
+    // Create Scores button
+    this.scoresButton = new Button('Scores');
+    this.scoresButton.onClick(() => {
+      this.__DOM_SCORES__.style.display = 'flex';
+      loadSecondaryPanel();
+    })
+    this.__DOM__.appendChild(this.scoresButton.__DOM__);
+    // Create Back button
+    const backButtonScores = new Button('Retour');
+    backButtonScores.onClick(() => {
+      hideSecondaryPanel();
+    })
+    this.__DOM_SCORES__.appendChild(backButtonScores.__DOM__);
+
+    // Create Credits DOM
+    this.__DOM_CREDITS__ = document.createElement('div');
+    this.__DOM_CREDITS__.style.display = 'none';
+    this.__DOM_CREDITS__.style.flexDirection = 'column';
+    this.__DOM_CREDITS__.style.alignItems = 'center';
+    this.__DOM_CREDITS__.style.justifyContent = 'center';
+    this.__DOM_CREDITS__.style.gap = '20px';
+    const creditTitle = document.createElement('div');
+    // Title
+    creditTitle.innerHTML = 'Crédits';
+    this.__DOM_CREDITS__.appendChild(creditTitle);
+    this.__DOM__.appendChild(this.__DOM_CREDITS__);
+    // Content
+    const content = document.createElement('div');
+    content.style.fontSize = '20px';
+    content.innerHTML = `
+    Développé par <a href="https://github.com/gugustinette" target="_blank">Augustin MERCIER</a> et <a href="https://github.com/oulahoop" target="_blank">Maël CAUBERE</a><br>
+    <br>
+    Musiques par <a href="https://github.com/gugustinette" target="_blank">Augustin MERCIER</a>
+    <br>
+    <br>
+    Assets graphiques :<br>
+    - <a href="https://kenney.nl/assets/space-kit" target="_blank">https://kenney.nl/assets/space-kit</a><br>
+    - <a href="https://kenney.nl/assets/space-station-kit" target="_blank">https://kenney.nl/assets/space-station-kit</a><br>
+    - <a href="https://fliflifly.itch.io/hearts-and-health-bar" target="_blank">https://fliflifly.itch.io/hearts-and-health-bar</a><br>
+    <br>
+    SFX : <a href="https://samplefocus.com/" target="_blank">https://samplefocus.com/</a>
+    <br>
+    <br>
+    Moteur de jeu : <a href="https://fibbo.dev" target="_blank">Fibbo</a>
+    `;
+    this.__DOM_CREDITS__.appendChild(content);
+    // Create Credits button
+    this.creditsButton = new Button('Crédits');
+    this.creditsButton.onClick(() => {
+      this.__DOM_CREDITS__.style.display = 'flex';
+      loadSecondaryPanel();
+    })
+    this.__DOM__.appendChild(this.creditsButton.__DOM__);
+    // Create Back button
+    const backButtonCredits = new Button('Retour');
+    backButtonCredits.onClick(() => {
+      hideSecondaryPanel();
+    })
+    this.__DOM_CREDITS__.appendChild(backButtonCredits.__DOM__);
 
     // Append to body
     document.body.appendChild(this.__DOM__);
